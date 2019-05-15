3b29391105305416c74ef15c0040f85a3cf8aa51
Galpil_Laptop <galan.pil@gmail.com>
2019-05-15 09:18:59 +0700

    updating the model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_11_103448) do
+ActiveRecord::Schema.define(version: 2019_05_12_152034) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
